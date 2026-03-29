8a08e1d5bed0fe14cba621158c04f6687a98ad3f
Pragadishwaran KP <pragadishwaran22@gmail.com>
2026-03-29 19:07:07 +0530

    created a react simple react agent

--- react_agent_basics.py
+++ react_agent_basics.py
@@ -1,6 +1,29 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain.agents import create_agent
+from langchain.agents import initialize_agent
 from langchain_core.tools import tool
 from langchain_community.tools import TavilySearchResults
 import datetime
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+search_tool = TavilySearchResults(search_depth="basic")
+
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
+    """Returns the current date and time"""
+    return datetime.datetime.now().strftime(format)
+
+tools = [search_tool, get_system_time]
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent="zero-shot-react-description",
+    verbose=True
+)
+
+response = agent.invoke("When was SpaceX's last launch?")
+print(response)
