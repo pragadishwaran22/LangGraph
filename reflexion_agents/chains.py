acc4ab69cfa615785a05b0f30849f98a313aec65
Pragadishwaran KP <pragadishwaran22@gmail.com>
2026-04-07 19:10:49 +0530

    successfully created revisor_chain

--- reflexion_agents/chains.py
+++ reflexion_agents/chains.py
@@ -2,14 +2,17 @@ from langchain.messages import HumanMessage
 import datetime
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
-from schema import AnswerQuestion
+from schema import AnswerQuestion, Reflection, ReviseAnswer
 from langchain_core.output_parsers import PydanticOutputParser
 from dotenv import load_dotenv
 
 load_dotenv()
 
-pydantic_parser = PydanticOutputParser(pydantic_object=AnswerQuestion)
-format_instructions = pydantic_parser.get_format_instructions()
+pydantic_parser_responder = PydanticOutputParser(pydantic_object=AnswerQuestion)
+format_instructions_1 = pydantic_parser_responder.get_format_instructions()
+
+pydantic_parser_revisor = PydanticOutputParser(pydantic_object=ReviseAnswer)
+format_instruction_2 = pydantic_parser_revisor.get_format_instructions()
 
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
@@ -30,22 +33,41 @@ Return ONLY valid JSON.
     MessagesPlaceholder(variable_name="messages"),
     ("system", "Answer the user's question above using the required format."),
 ]
-).partial(
-time=lambda: datetime.datetime.now().isoformat(),
-format_instructions=format_instructions
-)
+).partial(time=lambda: datetime.datetime.now().isoformat())
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
 
 first_responder_prompt_template = actor_prompt_template.partial(
-    first_instruction = "provide a detailed -250 word answer"
+    first_instruction = "provide a detailed -250 word answer",
+    format_instructions=format_instructions_1
+)
+
+revisor_prompt_template = actor_prompt_template.partial(format_instructions = format_instruction_2,
+first_instruction = revise_instructions
 )
 
 llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
 
-first_responder_chain = first_responder_prompt_template | llm | pydantic_parser 
+first_responder_chain = first_responder_prompt_template | llm | pydantic_parser_responder
+revisor_chain = revisor_prompt_template | llm | pydantic_parser_revisor
 
+
+query = "write me a blog on how small bussiness can leverage the use of AI for their growth"
 response = first_responder_chain.invoke({
-    "messages": [HumanMessage(content="write me a blog on how small bussiness can leverage the use of AI for their growth")]
+    "messages": [HumanMessage(content=query)]
 })
-print(response.reflection)
+print(response)
+revised = revisor_chain.invoke({
+    "messages":[HumanMessage(content = query),
+    HumanMessage(content = response.answer)]
+})
+print(revised)
 
 
